2dc69a696d9bb30490c9afb4831f62b39e2e6451
Dmytro <sunsn4ke@gmail.com>
2022-07-06 08:56:00 +0300

    ready 4

--- src/index.js
+++ src/index.js
@@ -1,51 +1,16 @@
 module.exports = function toReadable(number) {
-  const digitToWord = (n) => {
-    switch (n) {
-      case '1':
-        return 'one';
-      case '2':
-        return 'two';
-      case '3':
-        return 'three';
-      case '4':
-        return 'four';
-      case '5':
-        return 'five';
-      case '6':
-        return 'six';
-      case '7':
-        return 'seven'
-      case '8':
-        return 'eight';
-      case '9':
-        return 'nine';
-      default:
-        return '';
-    }
+  if (number === 0) return 'zero';
+  const a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let str = '';
+  if (number > 99) {
+    str += a[Math.floor(number / 100)] + ' hundred ';
+    number = +number.toString().slice(1);
   }
-
-  let arr = number.toString().split('')
-                    .map((item, i) => i === 0 ? digitToWord(item) + ' hundred' : i === 1 ? digitToWord(item) + 'ty' : digitToWord(item))
-                    .map(item => item === 'fourty' ? 'forty' : item)
-                    .map(item => item === 'threety' ? 'thirty' : item)
-                    .map(item => item === 'twoty' ? 'twenty' : item)
-                    .map(item => item === 'fivety' ? 'fifty' : item)
-                    .map(item => item === 'eightty' ? 'eighty' : item)
-                    .join(' ');
-
-  const secondConvert = (string) => {
-    return string.includes('onety one') ? string.replace('onety one', 'eleven') :
-    string.includes('onety two') ? string.replace('onety two', 'twelve') :
-    string.includes('onety three') ? string.replace('onety three', 'thirteen') :
-    string.includes('onety four') ? string.replace('onety four', 'fourteen') :
-    string.includes('onety five') ? string.replace('onety five', 'fifteen') :
-    string.includes('onety six') ? string.replace('onety six', 'sixteen') :
-    string.includes('onety seven') ? string.replace('onety seven', 'seventeen') :
-    string.includes('onety eight') ? string.replace('onety eight', 'eighteen') :
-    string.includes('onety nine') ? string.replace('onety nine', 'nineteen') :
-    string.includes(' ty') ? string.replace(' ty', '') :
-    string;
+  if (number > 19) {
+    str += b[Math.floor(number / 10)] + ' '
+    number = +number.toString().slice(1);
   }
-
-  return secondConvert(arr);
+  str += a[number];
+  return str.trim();
 }
